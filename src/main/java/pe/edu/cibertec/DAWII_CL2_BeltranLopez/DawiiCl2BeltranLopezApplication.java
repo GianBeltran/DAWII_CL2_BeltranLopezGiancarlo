b381cec8a174bff006892c6a790fa0ef8970cc54
GianBeltran <Gbeltranl0506@gmail.com>
2023-10-28 19:17:56 -0500

    Creacion de proyecto

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL2_BeltranLopez/DawiiCl2BeltranLopezApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_BeltranLopez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2BeltranLopezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2BeltranLopezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_CL2_BeltranLopez/DawiiCl2BeltranLopezApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_BeltranLopez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2BeltranLopezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
